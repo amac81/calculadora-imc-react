cf7a5439e00db7cf785ca1456748144d09c03cf6
amac81 <arnaldocanelas@gmail.com>
2023-11-26 12:15:00 +0000

    data.js import

---
+++ imc-calc/src/data/data.js
@@ -0,0 +1,43 @@
+// IMC DATA
+export const data = [
+    {
+      min: 0,
+      max: 18.4,
+      classification: "Menor que 18,5",
+      info: "Magreza",
+      obesityDegree: "0",
+      infoClass: "low",
+    },
+    {
+      min: 18.5,
+      max: 24.9,
+      classification: "Entre 18,5 e 24,9",
+      info: "Normal",
+      obesityDegree: "0",
+      infoClass: "good",
+    },
+    {
+      min: 25,
+      max: 29.9,
+      classification: "Entre 25,0 e 29,9",
+      info: "Pré-Obeso",
+      obesityDegree: "I",
+      infoClass: "medium",
+    },
+    {
+      min: 30,
+      max: 39.9,
+      classification: "Entre 30,0 e 39,9",
+      info: "Obesidade",
+      obesityDegree: "II",
+      infoClass: "high",
+    },
+    {
+      min: 40,
+      max: 99,
+      classification: "Maior que 40,0",
+      info: "Obesidade grave",
+      obesityDegree: "III",
+      infoClass: "high",
+    },
+  ];
