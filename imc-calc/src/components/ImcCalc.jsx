1de4bf52321eeffd5c543e803b2064c9c5e10f4f
amac81 <arnaldocanelas@gmail.com>
2023-11-26 12:28:01 +0000

    ImcCalc component basic html structure

--- imc-calc/src/components/ImcCalc.jsx
+++ imc-calc/src/components/ImcCalc.jsx
@@ -1,9 +1,36 @@
 import "./ImcCalc.css"
 
 const ImcCalc = () => {
+    
   return (
-    <div>
-        
+    <div id="calc-container">
+        <h2>Calculadora de IMC</h2>
+        <form id="imc-form">
+            <div id="form-inputs">
+                <div className="form-control">
+                    <label htmlFor="height">A sua altura:</label>
+                    <input 
+                        type="text"
+                        name="height"
+                        id="height"
+                        placeholder="Exemplo: 1,76" 
+                    />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="weight">O seu peso:</label>
+                    <input 
+                        type="text"
+                        name="weight"
+                        id="weight"
+                        placeholder="Exemplo: 68" 
+                    />
+                </div>
+            </div>
+            <div className="action-control">
+                <button>Calcular</button>
+                <button>Limpar</button>
+            </div>
+        </form>
     </div>
   )
 }
